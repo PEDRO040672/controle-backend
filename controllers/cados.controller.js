8ee10b9493de7560c61c96b125dceb9de57dd395
Pedro Azevedo <asa_pedro@hotmail.com>
2026-03-27 00:25:28 -0300

    27/03/2026 as 00:25

--- controllers/cados.controller.js
+++ controllers/cados.controller.js
@@ -3,7 +3,28 @@ const pool = require('../db/pool');
 // GET /cados
 exports.listar = async (req, res) => {
   try {
-    const result = await pool.query('SELECT * FROM cados ORDER BY os_data');
+    //const result = await pool.query('SELECT * FROM cados ORDER BY os_data');
+    const result = await pool.query(
+      `SELECT 
+        cados.*,
+        cadhis.his_desc,
+        cadcid.cid_nome,
+        cadtit.tit_nome,
+        cadeqp.eqp_desc,
+        cadope.ope_nome
+      FROM cados
+      LEFT JOIN cadhis 
+        ON cadhis.his_id = cados.os_his
+      LEFT JOIN cadcid 
+        ON cadcid.cid_id = cados.os_cid
+      LEFT JOIN cadtit 
+        ON cadtit.tit_id = cados.os_tit
+      LEFT JOIN cadeqp 
+        ON cadeqp.eqp_id = cados.os_eqp
+      LEFT JOIN cadope 
+        ON cadope.ope_id = cados.os_ope
+      ORDER BY cados.os_data DESC`
+    );
     res.json(result.rows);
   } catch (error) {
     console.error(error);
@@ -16,7 +37,25 @@ exports.buscarPorOs_tr = async (req, res) => {
   const { os_tr } = req.params;
   try {
     const result = await pool.query(
-      'SELECT * FROM cados WHERE os_tr = $1',
+      `SELECT 
+        cados.*,
+        cadhis.his_desc,
+        cadcid.cid_nome,
+        cadtit.tit_nome,
+        cadeqp.eqp_desc,
+        cadope.ope_nome
+      FROM cados
+      LEFT JOIN cadhis 
+        ON cadhis.his_id = cados.os_his
+      LEFT JOIN cadcid 
+        ON cadcid.cid_id = cados.os_cid
+      LEFT JOIN cadtit 
+        ON cadtit.tit_id = cados.os_tit
+      LEFT JOIN cadeqp 
+        ON cadeqp.eqp_id = cados.os_eqp
+      LEFT JOIN cadope 
+        ON cadope.ope_id = cados.os_ope
+        WHERE cados.os_tr = $1`,
       [os_tr]
     );
     if (result.rowCount === 0) {
@@ -31,8 +70,8 @@ exports.buscarPorOs_tr = async (req, res) => {
 
 // POST /cados
 exports.criar = async (req, res) => {
-  const { os_tr } = req.params;
-  const { os_os, os_situ, os_data, os_hora, os_his, os_cid, os_tit, os_eqp, os_ope, os_obs, os_htkmi, os_htkmf, os_qtd, os_vlunit, os_vldesc, os_vltots } = req.body;
+  //const { os_tr } = req.params;
+  const { os_tr, os_os, os_situ, os_data, os_hora, os_his, os_cid, os_tit, os_eqp, os_ope, os_obs, os_htkmi, os_htkmf, os_qtd, os_vlunit, os_vldesc, os_vltots } = req.body;
   try {
     const result = await pool.query(
       `INSERT INTO cados (os_tr, os_os, os_situ, os_data, os_hora, os_his, os_cid, os_tit, os_eqp, os_ope, os_obs, os_htkmi, os_htkmf, os_qtd, os_vlunit, os_vldesc, os_vltots)
